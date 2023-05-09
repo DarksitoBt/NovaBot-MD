a7d3566f47cd2a836e7d006b0927df940188c5ce
DarksitoBt <117957886+DarksitoBt@users.noreply.github.com>
2023-05-09 12:52:54 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       } catch (e) {
         console.error(e)
       } finally {
-      await conn.reply(m.chat, `⏳ *Pera caosa que ando kgando, ahorita te lo creo....*\n\n*ᴺᵒ ʰᵃᵍᵃ ˢᵖᵃᵐ*`, m)
+      await conn.reply(m.chat, `⏳ *Pera caosa que ando kgando, ahorita te lo creo....*\n\n*No hagas spam*`, m)
         if (!stiker) {
           if (/webp/g.test(mime)) out = await webp2png(img)
           else if (/image/g.test(mime)) out = await uploadImage(img)
