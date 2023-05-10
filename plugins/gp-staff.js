3b2a1a2e04ae2c1e5201acb14b083a52d500a869
DarksitoBt <117957886+DarksitoBt@users.noreply.github.com>
2023-05-10 00:52:23 -0500

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, participants, groupMetadata, args }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || '' //Atras va la url de la imagen
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://raw.githubusercontent.com/DarksitoBt/NovaBot-MD/main/src/e05904e2-daf1-4f49-b2f7-c5e8fdd0aad4.jpg' //Atras va la url de la imagen
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n╠ 🔸️ ')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
